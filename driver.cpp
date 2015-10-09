2031bfd9388464db1c6dd3b88c564f81dfaba970
Kristine Laranjo <kristineflaranjo@gmail.com>
2015-10-08 23:22:50 -0700

    my first commit

---
+++ driver.cpp
@@ -0,0 +1,19 @@
+// Kristine Laranjo, Emily Garcia, Kori Gann, Mavey Ma
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cassert>
+
+using namespace std;
+
+char menu();
+double climbing();
+double scuba();
+double skyDive();
+double spelunk();
+
+int main()
+{
+
+    return 0;
+}
